55556a06c3aa46861bc69c241f372f77418eba2b
marto711 <martram711@gmail.com>
2023-12-08 21:10:22 -0300

    primer script con html

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "<html>
+    <head>
+        <title>Resultados nmap</title>
+    </head>
+    <body>
+        <h1>Resultados nmap</h1>
+    	<p1>Aqui van los resultados de nmap</p1>
+     </body>
+</html>"
+    
