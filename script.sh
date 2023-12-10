7c458a0e78fba6d394c83d9b78db0cb35c9d63c4
marto711 <martram711@gmail.com>
2023-12-10 09:01:25 -0300

    if

--- script.sh
+++ script.sh
@@ -10,4 +10,14 @@ echo "<html>
     	<p1>Aqui van los resultados de nmap</p1>
      </body>
 </html>"
+
+if false; then
+	echo "la posta es posta"
+else
+	echo "la posta no es posta"
+
+fi
+
+#expr es lo que se va a evaluar si es verdadero o falso, luego va el then (entonces)
+#lo importante está en "expr"
     
