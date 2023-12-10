088b87a464c9b79eb9854222ec7ee9a4a6777b4c
marto711 <martram711@gmail.com>
2023-12-10 11:05:31 -0300

    parametros,  y

--- script.sh
+++ script.sh
@@ -4,12 +4,14 @@ TITULO="Resultados nmap"
 FECHA_ACTUAL="$(date)"
 TIMESTAMP="Informe generado el $FECHA_ACTUAL por el usuario $USERNAME"
 
+#Genera una serie de comandos con nmap, ejecuta la serie.
 nmap_exec () {
-	echo "[INFO] Ejecutnado nmap, banca un toque..."
-	sudo nmap -sV 192.168.1.0/24 > salida_nmap.raw
+	echo "[INFO] Ejecutnado nmap de la red $1, banca un toque..."
+	sudo nmap -sV $1 > $2
         echo "[OK] Fichero salida_nmap.raw generado correctamente"	
 }
 
+#Toma las lineas de codigo HTML y las mete en una función para usarla en otros momentos.
 generar_html (){
 cat <<EOF
 <html>
@@ -26,14 +28,14 @@ cat <<EOF
 EOF
 }
 
-#Reporte de nmap
-nmap_exec
+#Ejecuta la función nmap_exec que contiene el inicio de la acción, la acción y la salida en un archivo .raw
+nmap_exec "192.168.1.0/24" "salida_nmap2.raw"
 
-
-#Reporte HTML
+#Termina la funcion anterior y lanza un mensaje de que dará un reporte en HTML
 echo "[INFO] Generando reporte HTML..."
 
-#Pasaje del reporte a HTML
+#Mete la función dentro de un .html
+#Agarra la funcion que aglomera al HTML y lo mete en un archivo
 generar_html > resultados_nmap.html 
 
 echo "[OK] Reporte resultados.html generado correctamente"
