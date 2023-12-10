9766da2e4a979d5a5a662e92bcb04f9f5eb1639b
marto711 <martram711@gmail.com>
2023-12-10 10:46:40 -0300

    funciones

--- script.sh
+++ script.sh
@@ -4,6 +4,13 @@ TITULO="Resultados nmap"
 FECHA_ACTUAL="$(date)"
 TIMESTAMP="Informe generado el $FECHA_ACTUAL por el usuario $USERNAME"
 
+nmap_exec () {
+	echo "[INFO] Ejecutnado nmap, banca un toque..."
+	sudo nmap -sV 192.168.1.0/24 > salida_nmap.raw
+        echo "[OK] Fichero salida_nmap.raw generado correctamente"	
+}
+
+generar_html (){
 cat <<EOF
 <html>
     <head>
@@ -17,7 +24,19 @@ cat <<EOF
 </html>
 
 EOF
+}
 
+#Reporte de nmap
+nmap_exec
+
+
+#Reporte HTML
+echo "[INFO] Generando reporte HTML..."
+
+#Pasaje del reporte a HTML
+generar_html > resultados_nmap.html 
+
+echo "[OK] Reporte resultados.html generado correctamente"
 numero=7
 
 #-le quiere decir menor o igual
