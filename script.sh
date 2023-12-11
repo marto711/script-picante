77b2b926c4e1b97b793f7780302c41d88f89f114
marto711 <martram711@gmail.com>
2023-12-11 08:36:56 -0300

    for

--- script.sh
+++ script.sh
@@ -5,48 +5,48 @@ FECHA_ACTUAL="$(date)"
 TIMESTAMP="Informe generado el $FECHA_ACTUAL por el usuario $USERNAME"
 
 #prueba de variable local, solo sirve dentro de la funcion
-funcion_pichi () {
-	local variable="variable local"
-	echo $variable
-	return
-}
+#funcion_pichi () {
+#	local variable="variable local"
+#	echo $variable
+#	return
+#}
 #Genera una serie de comandos con nmap, ejecuta la serie.
-nmap_exec () {
-	echo "[INFO] Ejecutnado nmap de la red $1, banca un toque..."
-	sudo nmap -sV $1 > $2
-        echo "[OKi] Fichero salida_nmap.raw generado correctamente"
-	exit	
-}
+#nmap_exec () {
+#	echo "[INFO] Ejecutnado nmap de la red $1, banca un toque..."
+#	sudo nmap -sV $1 > $2
+#        echo "[OKi] Fichero salida_nmap.raw generado correctamente"
+#	exit	
+#}
 
 #Toma las lineas de codigo HTML y las mete en una función para usarla en otros momentos.
-generar_html (){
-cat <<EOF
-<html>
-    <head>
-        <title>$TITULO</title>
-    </head>
-    <body>
-       <h1>$TITULO</h1>
-        <p1>Aqui van los resultados de nmap</p1>
-        <p2>$TIMESTAMP</p2>
-     </body>
-</html>
+#generar_html (){
+#cat <<EOF
+#<html>
+#    <head>
+#        <title>$TITULO</title>
+#    </head>
+#    <body>
+#       <h1>$TITULO</h1>
+#        <p1>Aqui van los resultados de nmap</p1>
+#        <p2>$TIMESTAMP</p2>
+#     </body>
+#</html>
 
-EOF
-}
+#EOF
+#}
 #probando funcion local
-funcion_pichi
+#funcion_pichi
 #Ejecuta la función nmap_exec que contiene el inicio de la acción, la acción y la salida en un archivo .raw
-nmap_exec "192.168.1.0/24" "salida_nmap2.raw"
+#nmap_exec "192.168.1.0/24" "salida_nmap2.raw"
 
 #Termina la funcion anterior y lanza un mensaje de que dará un reporte en HTML
-echo "[INFO] Generando reporte HTML..."
+#echo "[INFO] Generando reporte HTML..."
 
 #Mete la función dentro de un .html
 #Agarra la funcion que aglomera al HTML y lo mete en un archivo
-generar_html > resultados_nmap.html 
+#generar_html > resultados_nmap.html 
 
-echo "[OK] Reporte resultados.html generado correctamente"
+#echo "[OK] Reporte resultados.html generado correctamente"
 numero=7
 
 #-le quiere decir menor o igual
@@ -59,16 +59,16 @@ numero=7
 
 ##Este if lo que hace es buscar el archivo que se genera con la funcion "nmap_exec" y consultar si se generé hace menos de 30 min, si coinciden
 ##las especdificaciones, me dirá que ya existe
-if [ $(find salida_nmap.raw -mmin -30) ]; then	
-	echo "[INFO] El fichero existe y tiene una antigûedad menor a 30 minutos"
+#if [ $(find salida_nmap.raw -mmin -30) ]; then	
+#	echo "[INFO] El fichero existe y tiene una antigûedad menor a 30 minutos"
 #elif [ 5 -le 10 ]; then 
 #	echo "la de arriba no se cumple, entonces me cumplo yo. Soy un else travesti"
 
 ## Si el if no se cumple, se generará ese archivo de salida, por ende si ejecuto el script ahora, me lo va a crear porque lo hice hace unas horas, 
 ## pero si lo vuelvo a ejecutar una vez hecho el archivo, ahi si me va a tirar el mensaje de que ya existe
-else
-	nmap_exec "192.168.1.0/24" "salida_nmap.raw"
-fi
+#else
+#	nmap_exec "192.168.1.0/24" "salida_nmap.raw"
+#fi
 
 #expr es lo que se va a evaluar si es verdadero o falso, luego va el then (entonces)
 #lo importante está en "expr"
@@ -96,5 +96,13 @@ else
         echo "no es cierto mi loco"
 fi
 
+######
+#FOR
 
+for efe in 2 5 7; do
+	echo "merequetengue, me parece que es un $efe"
+done
 
+for fotos in $(find /home/martin -name '*.jpg'); do
+	echo "Estas son las fotos del usuario: $fotos"
+done
